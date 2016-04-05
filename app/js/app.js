39bbce8585802b0d1ed93774cef3d9bf3fa56c21
Daniel <carba@kth.se>
2016-04-05 15:55:09 +0200

    Rearranging and getting angular to run on localhost

---
+++ app/js/app.js
@@ -0,0 +1,24 @@
+ 
+
+var laundryTimeApp = angular.module('laundryTime', ['ngRoute','ngResource']);
+
+laundryTimeApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.when('/home', {
+			templateUrl: 'templates/welcome.html'
+		}).
+		when('/info', {
+			templateUrl: 'templates/info.html'
+		}).
+		when('/residence/:res', {
+			templateUrl: 'templates/residenceOverview.html',
+			controller: 'overviewCtrl'
+		}).
+		otherwise({
+			redirectto: '/home'
+		})
+
+	}
+]);
+
+console.log("Laundry time configured")
